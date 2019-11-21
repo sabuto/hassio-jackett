b48155f2c07b3fae4bc0b4404bda4b0cd0bfad7f
Robert Dunne <robe_dunne@hotmail.com>
2019-11-21 14:49:12 +0000

    updated nginx again

--- jackett/rootfs/run.sh
+++ jackett/rootfs/run.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bashio
+declare port
+declare certfile
+declare hassio_dns
+declare ingress_interface
+declare ingress_port
+declare ingress_entry
+WAIT_PIDS=()
 
 bashio::log.info "starting...."
 
-WAIT_PIDS=()
+ingress_port=$(bashio::addon.ingress_port)
+ingress_interface=$(bashio::addon.ip_address)
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf
 
 exec nginx &
 WAIT_PIDS+=($!)
