e7fb6acdafd88c96b6e7713f3bd912a4890aa94c
Robert Dunne <robe_dunne@hotmail.com>
2019-11-21 14:32:50 +0000

    update testing

--- jackett/rootfs/run.sh
+++ jackett/rootfs/run.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bashio
 
 bashio::log.info "starting...."
+
+WAIT_PIDS=()
+
+exec nginx &
+WAIT_PIDS+=($!)
+
+function stop_addon() {
+	bashio::log.info "Kill Processes..."
+	kill -15 "${WAIT_PIDS[@]}"
+	bashio::log.info "Done"
+}
+
+trap "stop_addon" SIGTERM SIGHUP
+
+#Wait until all is done
+bashio::log.info "All is running smoothly"
+wait "${WAIT_PIDS[@]}"
