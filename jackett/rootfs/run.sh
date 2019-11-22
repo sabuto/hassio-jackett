5581dddccfe8227ad551950f27928eeb18b6e420
Robert Dunne <robe_dunne@hotmail.com>
2019-11-22 12:31:12 +0000

    updated folders

--- jackett/rootfs/run.sh
+++ jackett/rootfs/run.sh
@@ -12,9 +12,9 @@ bashio::log.info "starting...."
 ingress_entry=$(bashio::addon.ingress_entry)
 ingress_port=$(bashio::addon.ingress_port)
 ingress_interface=$(bashio::addon.ip_address)
-sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf.disabled
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf.disabled
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf.disabled
+sed -i "s/%%port%%/${ingress_port}/g" /etc/nginx/servers/ingress.conf
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/servers/ingress.conf
 
 hassio_dns=$(bashio::dns.host)
 sed -i "s/%%hassio_dns%%/${hassio_dns}/g" /etc/nginx/includes/resolver.conf
@@ -24,8 +24,19 @@ WAIT_PIDS+=($!)
 
 bashio::log.info "starting jackett"
 
-#exec /opt/Jackett/jackett --NoUpdates &
-#WAIT_PIDS+=($!)
+if ! bashio::fs.directory_exists '/config/jackett'; then
+	mkdir -p /config/jackett
+	mkdir -p /share/jackett
+fi
+
+if ! bashio::fs.file_exists '/config/jackett/ServerConfig.json'; then
+	mv /Jackett/ServerConfig.json /config/jackett/ServerConfig.json
+fi
+
+cd /opt/Jackett || bashio::exit.nok "setup gone wrong!"
+
+exec jackett --NoUpdates &
+WAIT_PIDS+=($!)
 
 function stop_addon() {
 	bashio::log.info "Kill Processes..."
