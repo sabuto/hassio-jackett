6381a39a01bfa89afa06f90b7f866808e8580624
Robert Dunne <robe_dunne@hotmail.com>
2019-11-25 16:22:47 +0000

    add travis lint back in

--- .travis/lint.sh
+++ .travis/lint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-docker pull hadolint/hadolint:v1.3.0
+docker pull hadolint/hadolint:v1.17.3
 
 echo "now linting: Dockerfile"
 docker run --rm -i hadolint/hadolint:v1.3.0 hadolint --ignore DL3006 - < "$(pwd)/jackett/Dockerfile"
